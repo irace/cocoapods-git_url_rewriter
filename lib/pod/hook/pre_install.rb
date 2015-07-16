13a1b91b0eb1f0c34134332048b9fdc5f9353104
Brian Michel <brian.michel@gmail.com>
2015-07-16 19:18:43 -0400

    use bang to indicate inplace changes

--- lib/pod/hook/pre_install.rb
+++ lib/pod/hook/pre_install.rb
@@ -1,4 +1,4 @@
 Pod::HooksManager.register(:pre_install) do |intaller_context|
     configuration = Pod::GitSourceURLRewriter::Configuration.new(intaller_context)
-    Pod::GitSourceURLRewriter::Rewriter.new(installer_context, configuration).rewrite
+    Pod::GitSourceURLRewriter::Rewriter.new(installer_context, configuration).rewrite!
 end
