002051b12441136fb19dc05b00f97f5d77569844
Brian Michel <brian.michel@gmail.com>
2015-07-17 11:34:33 -0400

    Removing config and solidifying logic

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,15 +1,6 @@
-# Top level module for source url rewriter
-module SourceURLRewriter
-  Pod::HooksManager.register('cocoapods-github_token_http_rewriter', :pre_install) do |context|
-    require 'cocoapods'
-    require 'pod/src/configuration'
-    require 'pod/src/rewriter'
-
-    Pod::UI.warn('Just about to set some stuff up')
-    configuration = SourceURLRewriter::Configuration.new(context)
-    SourceURLRewriter::Rewriter.new(context, configuration).rewrite!
-  end
-end
+require 'cocoapods'
+require 'pod/src/configuration'
+require 'pod/src/rewriter'
 
 module Pod
   module Downloader
@@ -17,14 +8,12 @@ module Pod
         alias_method :github_token_http_rewriter_url, :url
         # FIXME: This needs to be better encapsulated
         def url
-          if github_token_http_rewriter_url.start_with?("git@git.corp.yahoo.com") 
-            huh = github_token_http_rewriter_url.sub("git@git.corp.yahoo.com:", "https://47d02b88fc9cf9a3a0ba96692165558e8047f009@git.corp.yahoo.com/")
-            Pod::UI.info("Transformed a #{github_token_http_rewriter_url} into #{huh}")
-            return huh
-          end
-          return github_token_http_rewriter_url
+          source_url_rewriter.url_for(github_token_http_rewriter_url)
+        end
+
+        def source_url_rewriter
+          @source_url_rewriter ||= SourceURLRewriter::Rewriter.new
         end
     end
   end
 end
-
--- lib/pod/src/rewriter.rb
+++ lib/pod/src/rewriter.rb
@@ -1,21 +1,38 @@
 module SourceURLRewriter
-  # Url rewriter
+  # Used to provide rewritten urls
+  # People using this pod would install as follows
+  # plugin 'cocoapods-github_token_http_rewriter', {
+  #   "pattern" => "replacement"
+  # }
   class Rewriter
-    # Create a new rewriter with a context and config
-    def initialize(context, config)
-      @config = config
-      @context = context
-    end
+    def url_for(url) 
+      options = user_options
+      if options 
+        first_matching_transform_key = options.keys.select {|key| url.start_with?(key)}.first 
 
-    def rewrite!
-      deps.each do |deps|
+        if first_matching_transform_key
+          rewritten_url = url.sub(first_matching_transform_key, options[first_matching_transform_key])
+          Pod::UI.info("Transforming download URL #{url}} into #{rewritten_url}")
+          return rewritten_url
+        end
+      else
+        Pods::UI.notice("No options have been specified for rewriting, every url will pass through.")
       end
+
+      Pod::UI.info("No patterned matched, passing #{url} through")
+      return url
     end
 
     private
 
-    def deps
-      @deps ||= @context.podfile.dependencies
+    def user_options
+      options = podfile.plugins['cocoapods-github_token_http_rewriter']
+      options
     end
+
+    def podfile
+      Pod::Config.instance.podfile
+    end
+
   end
 end
